3dc583812471fc1a9e88dc17d1dbd2842aff4c86
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-02-28 11:26:02 +0400

    feature:Cooky was added

--- FronToBack/Controllers/BasketController.cs
+++ FronToBack/Controllers/BasketController.cs
@@ -6,15 +6,15 @@ public class BasketController:Controller
 {
     public IActionResult Index()
     {
-         HttpContext.Session.SetString("name","Asgar");
-        // Response.Cookies.Append("name","Asgar",new CookieOptions{MaxAge=TimeSpan.FromDays(1)});
+        //  HttpContext.Session.SetString("name","Asgar");
+         Response.Cookies.Append("name","Asgar",new CookieOptions{MaxAge=TimeSpan.FromDays(1)});
         return Content("set olundu");
     }
 
         public IActionResult GetItem()
     {
-       string name = HttpContext.Session.GetString("name");
-    //    string name = Request.Cookies["name"];
+    //    string name = HttpContext.Session.GetString("name");
+       string name = Request.Cookies["name"];
         return Content(name);
     }
 
