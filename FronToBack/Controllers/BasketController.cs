0851ff4207aa93ea32986767e39e2306bd203556
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-02-28 12:02:21 +0400

    feature:Add method was added

--- FronToBack/Controllers/BasketController.cs
+++ FronToBack/Controllers/BasketController.cs
@@ -1,9 +1,19 @@
+using FronToBack.Models;
 using Microsoft.AspNetCore.Mvc;
+using FronToBack.DAL;
 
 namespace FronToBack.Controllers;
 
 public class BasketController:Controller
 {
+
+private readonly AppDbContext _appDbContext;
+
+    public BasketController(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
     public IActionResult Index()
     {
         //  HttpContext.Session.SetString("name","Asgar");
@@ -17,6 +27,15 @@ public class BasketController:Controller
        string name = Request.Cookies["name"];
         return Content(name);
     }
+
+     public async Task<IActionResult>  Add(int id,string name)
+    {
+        if(id==null) return NotFound();
+        Product product = await _appDbContext.Products.FindAsync(id);
+        if(product==null) return NotFound();
+        return Json(product);
+    }
+
 
     
 }
